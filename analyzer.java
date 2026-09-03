990c715642f5654e86a6e9185dfc08e3d67a1b33
sohan321 <sohanomshrestha@gmail.com>
2026-09-03 10:56:22 -0500

    analyzer

---
+++ analyzer.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class analyzer {
+    public static void main(String[] args) {
+        String word = "";
+        int vowel = 0;
+        int consonants = 0;
+        int digit = 0;
+        int spaces = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        word = sc.nextLine();
+        word = word.toLowerCase();
+        int chars = word.length();
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                digit += 1;
+            }
+
+            else if (ch == ' ') {
+                spaces += 1;
+            }
+
+            else if (Character.isLetter(ch)) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowel += 1;
+                }
+
+                else {
+                    consonants += 1;
+                }
+            }
+
+        }
+
+        sc.close();
+        System.out.println();
+        System.out.println("Characters: " + chars);
+        System.out.println("Vowels: " + vowel);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digit);
+        System.out.println("Spaces: " + spaces);
+    }
+
+}
